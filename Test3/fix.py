73092b9b4d18c1f03791b8ab571ecdd3a685dddf
“Mehul <kaushikmehul94@gmail.com>
2025-04-10 00:37:20 +0530

    fixed

--- Test3/fix.py
+++ Test3/fix.py
@@ -1,7 +1,7 @@
 import nbformat
 
 # Load the notebook
-notebook_path = "Yolo_finetuning3.ipynb"
+notebook_path = "Generate_Images.ipynb"
 with open(notebook_path, "r", encoding="utf-8") as f:
     nb = nbformat.read(f, as_version=nbformat.NO_CONVERT)
 
@@ -10,7 +10,7 @@ if 'widgets' in nb['metadata']:
     del nb['metadata']['widgets']
 
 # Save the cleaned notebook
-fixed_path = "Yolo_finetuning_cleaned.ipynb"
+fixed_path = "Generate_Images1.ipynb"
 with open(fixed_path, "w", encoding="utf-8") as f:
     nbformat.write(nb, f)
 
